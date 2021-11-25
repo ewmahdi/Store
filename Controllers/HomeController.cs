382b3d5a985e0aeb4c08fae820a13a2d66f3cd2b
hamed <hamed8080@gmail.com>
2021-11-25 11:56:59 +0330

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Store.Models;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Store.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly StoreContext _storeContext;
+
+        public HomeController(ILogger<HomeController> logger, StoreContext storeContext) // injection
+        {
+            _logger = logger;
+            _storeContext = storeContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _storeContext.Products.ToListAsync();
+            if (Request.Query.ContainsKey("isUpdate")) {
+                var isUpdate = bool.Parse(Request.Query["isUpdate"]);
+                ViewBag.IsUpdate = isUpdate;
+            }
+            ViewBag.Products = products;
+            return View();
+        }
+
+        public IActionResult InsertProduct(int? id)
+        {
+            //var id = Request.Query["id"];
+
+            if (id != null)
+            {
+                //update
+                var product = _storeContext.Products.Find(id.Value);
+                ViewBag.Product = product;
+            }
+            else
+            {
+                //Insert
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult InsertProduct(string name, string desc, decimal price)
+        {
+            _storeContext.Products.Add(new Product { Name = name, Desc = desc, Price = price });
+
+            if (name.Length <= 50)
+            {
+                _storeContext.SaveChanges();
+                return Json(new { isSuccess = true });
+            }
+            else
+            {
+                return Json(new { isSuccess = false });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(int productId, string name, string desc, decimal price)
+        {
+            _storeContext.Products.Update(new Product { Id = productId, Name = name, Desc = desc, Price = price });
+
+            if (name.Length <= 50)
+            {
+                _storeContext.SaveChanges();
+                return Json(new { isSuccess = true });
+            }
+            else
+            {
+                return Json(new { isSuccess = false });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult DeleteProduct(int id)
+        {
+
+            var product = _storeContext.Products.Find(id);
+            if (product != null)
+            {
+                _storeContext.Products.Remove(product);
+                _storeContext.SaveChanges();
+                return Json(new { isSuccessDeleted = true });
+            }
+            else
+            {
+                return Json(new { isSuccessDeleted = false });
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StoreContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Store
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal FactorPrice { get; set; }
+
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public decimal Price { get; set; }
+    }
+}
